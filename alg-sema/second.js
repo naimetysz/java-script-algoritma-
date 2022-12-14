43bdc275e1419cabba4f6430df5b1bb453a45c02
naimetysz <nnaimetuysuz@gmail.com>
2022-12-14 14:40:00 +0300

    change

---
+++ alg-sema/second.js
@@ -0,0 +1,23 @@
+// Girilen bir sayının pozitif mi negatif mi olduğunu ekrana yazdıran algoritma ve akış şeması
+
+// Algoritma
+
+// Başlangıçta, girilen sayıyı saklayan bir değişken tanımlayın (sayı).
+// sayı değerini kontrol edin:
+// Eğer sayı 0'dan büyükse, "pozitif" yazdırın.
+// Eğer sayı 0'dan küçükse, "negatif" yazdırın.
+// Akış Şeması
+
+// Başlangıç
+// sayı değişkenini tanımla
+// sayı değerini kontrol et:
+// eğer sayı > 0 ise, "pozitif" yazdır
+// eğer sayı < 0 ise, "negatif" yazdır
+// Bitiş
+// Bu algoritma ve akış şeması, girilen bir sayının pozitif mi negatif mi olduğunu ekrana yazdırmak için kullanılabilir. Umarım yardımcı olmuştur.
+
+
+
+
+
+
--- easy.js
+++
@@ -1,2 +0,0 @@
-import { userValidation } from "./first-qestion/1-userv.js";
-console.log(userValidation("helloorld"))
