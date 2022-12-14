f8e8ac8c85ce09613818e10ebc4ed2ae9d207e9c
naimetysz <nnaimetuysuz@gmail.com>
2022-12-14 14:32:18 +0300

    commit eklendi

---
+++ akis-semalari/akis.js
@@ -0,0 +1,29 @@
+// sayı1 ve sayı2 değişkenlerini tanımla
+var sayı1 = 5;
+var sayı2 = 10;
+
+// sayı1 ve sayı2 değerlerini topla ve toplam değişkenine ata
+var toplam = sayı1 + sayı2;
+
+// toplam değerini ikiye böl ve ortalama değişkenine ata
+var ortalama = toplam / 2;
+
+// ortalama değerini ekrana yazdır
+console.log(ortalama);
+/// Merhaba, bu sorunun cevabını vermek için, aşağıda bir algoritma ve akış şeması sunacağım.
+
+// Algoritma
+
+// Başlangıçta, girilen iki sayıyı saklayan iki değişken tanımlayın (sayı1 ve sayı2).
+// İki sayıyı toplayın ve toplamı bir değişkene saklayın (toplam).
+// Toplamı ikiye bölün ve sonucu bir değişkene saklayın (ortalama).
+// Ortalama değerini ekrana yazdırın.
+// Akış Şeması
+
+// Başlangıç
+// sayı1 ve sayı2 değişkenlerini tanımla
+// sayı1 ve sayı2 değerlerini topla ve toplam değişkenine ata
+// toplam değerini ikiye böl ve ortalama değişkenine ata
+// ortalama değerini ekrana yazdır
+// Bitiş
+// Bu algoritma ve akış şeması, girilen iki sayının ortalamasını hesaplamak için kullanılabilir. Umarım yardımcı olmuştur.
