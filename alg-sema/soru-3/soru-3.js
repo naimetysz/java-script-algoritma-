67b6e5b6963b13ff94d6a41da8ebe621f5c17a74
naimetysz <nnaimetuysuz@gmail.com>
2022-12-15 11:39:01 +0300

    change

---
+++ alg-sema/soru-3/soru-3.js
@@ -0,0 +1,6 @@
+function hipotenus(a, b) {
+    return Math.sqrt(a * a + b * b);
+  }
+  
+  console.log(hipotenus(3, 4));  // 5
+///  Bu örnekte, hipotenus adında bir fonksiyon tanımlanmış ve bu fonksiyon iki tamsayı parametre alır. İlk olarak, bu fonksiyon içerisinde a ve b parametrelerinin karesi toplanarak bir dik kenar uzunluğu hesaplanır. Daha sonra, bu değer karekök alınarak hipotenüs uzunluğu hesaplanır ve sonuç olarak bu değer döndürülür. Örnekte gösterildiği gibi bu fonksiyonu kullanarak 3 ve 4 gibi değerleri parametre olarak verdikten sonra, bu fonksiyonun döndürdüğü değer 5 olarak ekrana yazdırılmıştır.
---
+++ alg-sema/soru.js/ornek.js
@@ -0,0 +1,82 @@
+// JavaScript Hipotenüs Hesaplama Örneği
+// JavaScript Sayısal Loto Örneği
+// JavaScript Varmısın Yokmusun Örneği
+// JavaScript Dizi İçindeki En Büyük En Küçük Sayıyı Bulma Örneği
+// JavaScript Dikdörtgen Alan ve Çevre Hesaplama Örneği
+// JavaScript Asal Sayı Kontrol Etme Örneği
+// JavaScript Daire Alanı ve Çevresini Hesaplama Örneği
+// JavaScript Faktoriyel Hesaplama Örneği
+// JavaScript While Döngüsü Örnekleri
+// JavaScript Metin İçinde Arama Yapma Örneği
+// JavaScript Switch Case Kullanımı Örneği
+// JavaScript Rastgele Sayı Üretme Örneği
+// JavaScript Girilen Sayının Negatif Pozitif Olduğunu Bulan Örnek
+// JavaScript Sayfa Arkaplan Rengini Rastgele Değiştirme Örneği
+// JavaScript Dizi Metotları Örnekleri
+// JavaScript Km-Mil Çevirme Örneği
+// JavaScript Derece-Fahrenayt Çevirme Örneği
+// JavaScript Gün İsmini Ekrana Yazdırma Örneği
+// JavaScript Sayı Tahmin Oyunu Örneği
+// JavaScript Console Log Kullanımı
+// JavaScript Kürenin Hacmini Hesaplama Örneği
+// JavaScript Üçgenin Alanını Hesaplama Örneği
+// JavaScript Çarpım Tablosu Örneği
+// JavaScript 3′ e ve 5′ e Tam Bölünebilme Örneği
+// JavaScript Karenin Alanı ve Çevresini Hesaplama Örneği
+// JavaScript Yazılı Ortalaması Hesaplama Örneği
+// JavaScript Try-Catch-Finally Kullanımı Örneği
+// JavaScript Div İçeriğini Temizleme Örneği
+// JavaScript Switch-Case Kullanımı Örneği
+// JavaScript Buttona tıklandığında Kod Çalıştırma Örneği
+// JavaScript events(olay) kullanımı Örnekleri
+// JavaScript For Döngüsü Karışık Örnekler
+// JavaScript Tarih ve Saat Yazdırma Örneği
+// JavaScript Fonksiyon Kullanımı Örneği
+// JavaScript Vize Final Hesaplama Örneği
+// JavaScript onInput Olay Kullanımı Örneği
+// JavaScript Kayan Yazı Örneği
+// JavaScript Kombinasyon Hesaplama
+// JavaScript radio Kullanarak Dört İşlem Örneği
+// JavaScript Toplama Çıkarma Çarpma Bölme Örneği
+// JavaScript Sayfayı Tam Ekran Yapma Örneği
+// JavaScript Fare Konumunu Öğrenme Örneği
+// JavaScript Object Kullanımı Örneği
+// JavaScript Kalan Karakter Bulma- Karakter Sayma Örneği
+// JavaScript Mükemmel Sayı Kontrol Etme Örneği
+// Basit Dijital Saat Yapımı
+// Basit Tooltip Yapımı
+// Sayı tek mi, çift mi
+// Sayfayı Yenileme Kodu
+// JavaScript İntergral Alma
+// Çemberin çevresini hesaplayan program
+// Kürenin alanını hesaplayan program.
+// window.setTimeOut Örneği
+// Dikdörtgenin alanını çevresini hesaplayan program.
+// Kilometre cinsinden uzunluğu mile çevirme
+// JavaScript Dört İşlem Matematik Oyunu
+// JavaScript: Butona Tıklayınca Sayıyı Arttırma / Azaltma
+// Üçgenin Alanını Hesaplama
+// JavaScript: Farenin Konumunu Öğrenme
+// JavaScript Arkaplanı rastgele Değiştirme (setInterval)
+// Javascript:Faktöriyel Hesaplama Örneği
+// JavaScript: Sayıyı Yazıya Çevirme
+// JavaScript Şifre Üretici Yapımı
+// 4 Renk Direnç Hesaplama
+// Kağıt, Makas, Taş Oyunu
+// Hesap Makinası Örneği
+// Sayısal Loto Örneği
+// Farenin Konumunu Öğrenme
+// Fareyi Takip Eden Kutu
+// Üçgenin Alanını Hesaplama
+// JavaScript Ay İsmini Ekrana Yazdırma
+// Butona tıkladığında sayfayı tam ekran yapma
+// JavaScript animateRequestFrame 
+// JavaScript ile Ortalama Hesaplama
+// Ses dosyasını oynatma, duraklatma ve sesini değiştirme
+// Try Catch Throw Örneği (11 haneli numara kontrol örneği)
+// Bilgisayarın tuttuğu sayıyı tahmin etme örneği
+// 1-100 arasındaki sayıların 3’e yada 5’e bölünenlerini yazdırma
+// Input Range Kullanım Örneği
+// Output Element Kullanımı ve Örneği
+// Diğer JavaScript If Else Örnekleri
+// Diğer JavaScript Form Örnekleri
